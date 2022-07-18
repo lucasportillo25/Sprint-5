30c22795ce0be2b20378fdabe7d2ac8bfcc978be
lucasportillo25 <86985208+lucasportillo25@users.noreply.github.com>
2022-07-18 01:08:35 -0300

    Funciones

---
+++ App.py
@@ -0,0 +1,6 @@
+from cliente import Cliente
+from creacion_html import CreacionHTML
+from loadFile import Parser2
+
+cliente = Cliente()
+
---
+++ LoadFile.py
@@ -0,0 +1,14 @@
+from cliente import Cliente
+import json
+
+
+cliente = Cliente()
+
+class Parser2:
+
+    def leerDatos(self, filename:str):
+        with open(filename) as jsonFile:
+            clientes = json.load(jsonFile)
+
+        # cliente.inicializar(datos)
+        return (clientes)
