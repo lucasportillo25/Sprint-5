dfc1e833b1ddb905148640cde0a9b0f1da0e12ef
lucasportillo25 <86985208+lucasportillo25@users.noreply.github.com>
2022-07-18 01:09:32 -0300

    App

--- App.py
+++ App.py
@@ -4,3 +4,20 @@ from loadFile import Parser2
 
 cliente = Cliente()
 
+if __name__ == "__main__":
+
+    razones = ""
+
+    parse = Parser2()
+    datos = parse.leerDatos('eventos.json')
+    transacciones = datos['transacciones']
+
+    if datos['tipo'] == "CLASSIC":
+        motivos = cliente.ClienteClassic(transacciones, razones)
+    elif datos['tipo'] == "GOLD":
+        motivos = cliente.ClienteGold(transacciones, razones)
+    elif datos['tipo'] == "BLACK":
+        motivos = cliente.ClienteBlack(transacciones, razones)
+    
+    salida = CreacionHTML()
+    salida.crear_html(datos, motivos)
